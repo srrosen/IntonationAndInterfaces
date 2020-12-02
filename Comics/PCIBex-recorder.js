1d5c66c70eb3eb192b8c00995ee63661d959c355
samincharepoo <63875334+samincharepoo@users.noreply.github.com>
2020-12-01 20:05:15 -0500

    Update PCIBex-recorder.js

--- Comics/PCIBex-recorder.js
+++ Comics/PCIBex-recorder.js
@@ -6,35 +6,155 @@
 *
 */
 
+
 // Initiates PennController
 PennController.ResetPrefix(null);
 
 
 // Sets the order of events
 Sequence( 
-    "intro" 
-    ,
-    "recordTrial"
-)
+        "intro" 
+        ,
+        "recordTrial"
+    )
 
 
 // Gets the audio recorder ready
 InitiateRecorder("https://plinglab.princeton.edu/IBEX/exptA/exptA-up.php").label("intro")
 
+//introduction and instructions
+NewTrial(
+newImage("2fishRoundTank.png")
+    .print(),
+newImage("2fishRoundTank.png")
+    .print(),
+newImage("2fishRoundTank.png")
+    .print(),
+newImage("2fishRoundTank.png")
+    .print(),
+newImage("2fishRoundTank.png")
+    .print(),
+newImage("2fishRoundTank.png")
+    .print(),
+newImage("2fishRoundTank.png")
+    .print(),
+newImage("2fishRoundTank.png")
+    .print(),
+newImage("2fishRoundTank.png")
+    .print(),
+)   
 
-// The actual recording trials
-newTrial("recordTrial",
-    newText("Please record yourself saying goodbye happily").print()
-    ,
-    newMediaRecorder("happiness", "audio").print().log().wait()
-)
+Template( variable => 
+    newTrial("recordTrial",
+    // The actual recording trials and comics start here
+         newImage(variable.ImageFile)
+         .print(),
+         newButton("Next")
+        .print()
+        .wait(),
+        newMediaRecorder("happiness", "audio").print().log().wait()
+        newImage(variable.ImageFile)
+         .print(),
+         newButton("Next")
+        .print()
+        .wait(),
+        newMediaRecorder("happiness", "audio").print().log().wait()
+        newImage(variable.ImageFile)
+         .print(),
+         newButton("Next")
+        .print()
+        .wait(),
+        newMediaRecorder("happiness", "audio").print().log().wait()
+        newImage(variable.ImageFile)
+         .print(),
+         newButton("Next")
+        .print()
+        .wait(),
+        newMediaRecorder("happiness", "audio").print().log().wait()
+        newImage(variable.ImageFile)
+         .print(),
+         newButton("Next")
+        .print()
+        .wait(),
+        newMediaRecorder("happiness", "audio").print().log().wait()
+        newImage(variable.ImageFile)
+         .print(),
+         newButton("Next")
+        .print()
+        .wait(),
+        newMediaRecorder("happiness", "audio").print().log().wait()
+        newImage(variable.ImageFile)
+         .print(),
+         newButton("Next")
+        .print()
+        .wait(),
+        newMediaRecorder("happiness", "audio").print().log().wait()
+        newImage(variable.ImageFile)
+         .print(),
+         newButton("Next")
+        .print()
+        .wait()
+        newMediaRecorder("happiness", "audio").print().log().wait()
+        newImage(variable.ImageFile)
+         .print(),
+         newButton("Next")
+        .print()
+        .wait(),
+        newMediaRecorder("happiness", "audio").print().log().wait()
+        newImage(variable.ImageFile)
+         .print(),
+         newButton("Next")
+        .print()
+        .wait(),
+        newMediaRecorder("happiness", "audio").print().log().wait()
+        newImage(variable.ImageFile)
+         .print(),
+         newButton("Next")
+        .print()
+        .wait(),
+        newMediaRecorder("happiness", "audio").print().log().wait()
+        newImage(variable.ImageFile)
+         .print(),
+         newButton("Next")
+        .print()
+        .wait(),
+        newMediaRecorder("happiness", "audio").print().log().wait()
+        newImage(variable.ImageFile)
+         .print(),
+         newButton("Next")
+        .print()
+        .wait()
+        newMediaRecorder("happiness", "audio").print().log().wait()
+        newImage(variable.ImageFile)
+         .print(),
+         newButton("Next")
+        .print()
+        .wait(),
+        newMediaRecorder("happiness", "audio").print().log().wait()
+        newImage(variable.ImageFile)
+         .print(),
+         newButton("Next")
+        .print()
+        .wait(),
+        newMediaRecorder("happiness", "audio").print().log().wait()
+        newImage(variable.ImageFile)
+         .print(),
+         newButton("Next")
+        .print()
+        .wait(),
+        newMediaRecorder("happiness", "audio").print().log().wait()
+    )
 
-newTrial("recordTrial", 
-    newText("Please record yourself saying goodbye angrily").print()
-    ,
-    newMediaRecorder("sadness", "audio").print().log().wait()
-)
+newTrial( "final" ,
+newText("<p>Thank you for your participation!</p>")
+        .print()
+,
+   // newTrial("recordTrial", 
+     //   newText("Please record yourself saying goodbye angrily").print()
+       // ,
+        //ewMediaRecorder("sadness", "audio").print().log().wait()
+    )
 
 
-// Uploads the recordings
-UploadRecordings("sendAsync", "noblock")
+    // Uploads the recordings
+    UploadRecordings("sendAsync", "noblock")
